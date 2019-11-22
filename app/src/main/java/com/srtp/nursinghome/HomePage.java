13adcf6358de383a68ea0daf1452f11fc849701d
Silverdewy <1246213615@qq.com>
2019-11-23 00:27:22 +0800

    homepage and navigation

---
+++ app/src/main/java/com/srtp/nursinghome/HomePage.java
@@ -0,0 +1,13 @@
+package com.srtp.nursinghome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
